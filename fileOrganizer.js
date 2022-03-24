9a003e93c7bc810b9e7f2f8800844683afdfed10
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-25 00:28:47 +0530

    input

--- fileOrganizer.js
+++ fileOrganizer.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+// console.log("Hello world");
+let inputArr = process.argv;
+console.log(inputArr);
+//['pathToNode' , 'pathToFile' , 'input_passed_in_terminal']
+let input =inputArr[2];
+console.log(input);
